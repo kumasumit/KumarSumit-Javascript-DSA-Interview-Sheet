785112d857ecbdc5d3df13aa61b51f6a48fe59cc
Kumar Sumit <kumarsumit1989@gmail.com>
2022-07-16 02:31:53 +0530

    Day 1 16 July 2022 2 Array Questions Done

---
+++ Arrays/sumZero.js
@@ -0,0 +1,28 @@
+// Q. Given an input sorted Array, find first Pair whose sum is zero.
+// I/P: [-5, -4, -3, -2, -1, 0, 1,2, 3, 4, 5] A Sorted Array
+// O/P: [-5,5] return the first Pair whose sum is zero
+let arr =[-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
+function getSumZero(arr){
+    // 1. First Method
+    // for(let i=0;i<arr.length;i++)
+    // {
+    //     for(let j=0;j<arr.length;j++){
+    //         if(arr[i]+arr[j]===0){
+    //             return [arr[i], arr[j]];
+    //         }
+    //     }
+    // }
+
+    // 2. Second Method
+     for (let number of arr){
+        console.log("Outer Loop")
+        for (let j=1;j<arr.length;j++){
+            console.log("Inner Loop")
+            if(number + arr[j] === 0){
+                return [number, arr[j]];
+            }
+        }
+     }
+}
+console.log(getSumZero(arr));
+// this is of Object(n^2);
---
+++ Arrays/sumZeroOptimized.js
@@ -0,0 +1,24 @@
+// Q. Given an input sorted Array, find first Pair whose sum is zero.
+// I/P: [-5, -4, -3, -2, -1, 0, 1,2, 3, 4, 5] A Sorted Array
+// O/P: [-5,5] return the first Pair whose sum is zero
+//Do this is O(n) using Only one for loop.
+
+let arr =[-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
+function getSumZeroOptimized(arr){
+   let leftIndex = 0;
+   let rightIndex = arr.length-1;
+   while(leftIndex<rightIndex){
+    let sum = arr[leftIndex]+arr[rightIndex];
+    if(sum === 0){
+        return [arr[leftIndex], arr[rightIndex]];
+    }else if(sum>0){
+        rightIndex--;
+    }else if(sum<0){
+        leftIndex++
+    }
+  
+   }    
+}
+console.log(getSumZeroOptimized(arr));
+
+// This is done in O(n)
